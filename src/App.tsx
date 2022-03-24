d59877e65b69db430f7bb7b0c0c063dd0233465c
Oleg Chursin <olegchursin@gmail.com>
2022-03-24 13:06:38 -0400

    feat: v0.0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import {
+    UuiBaseTheme, UuiButton, UuiDefaultTheme, UuiGlobalVariablesTheme, UuiInverseTheme
+} from '@aonunited/react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<UuiBaseTheme />
+			<UuiDefaultTheme />
+			<UuiGlobalVariablesTheme />
+			<UuiInverseTheme />
+
+			<div className="App">
+				<h1>UUI TS</h1>
+
+				<UuiButton>Hello there!</UuiButton>
+			</div>
+		</>
+	);
 }
 
 export default App;
